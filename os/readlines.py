dd41ec06596d8547d029b22ff47109a63c69739e
AoEiuV020 <aoeiuv020@gmail.com>
2017-09-07 16:01:55 +0800

    Test readlines
    这个方法在dir和help看不到，

---
+++ os/readlines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+f = os.popen('echo hello')
+assert type(f) == os._wrap_close
+# 这个readlines方法在dir(f)和help(f)都没有提到，
+l = f.readlines()
+assert type(l) == list
