7f087ea886412acc8ee7dbfe565f3d9cf1e67b5f
AoEiuV020 <aoeiuv020@gmail.com>
2021-01-19 01:03:57 +0800

    测试exec运行另一个python脚本

--- getting_started/exec.py
+++ getting_started/exec.py
@@ -20,3 +20,15 @@ l = {}
 exec('global a; a, b = 1, 2', g, l)
 assert g['a'] == 1
 assert l['b'] == 2
+
+import sys
+
+from input import Provider
+from print import Matcher
+
+sys.stdin = Provider('2 2\n')
+sys.stdout = Matcher('4\n', flashable=True)
+exec(open('exec_test_script').read())
+sys.stdin = Provider('4 4\n')
+sys.stdout = Matcher('256\n', flashable=True)
+exec(open('exec_test_script').read())
