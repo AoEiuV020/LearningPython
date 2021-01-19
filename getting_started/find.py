a3344e1ddc20ebf604e984058e0493682f7cb297
AoEiuV020 <aoeiuv020@gmail.com>
2021-01-19 23:51:58 +0800

    测试find
    和index区别就是找不到有-1返回，

---
+++ getting_started/find.py
@@ -0,0 +1,7 @@
+assert 'asdf'.find('d') == 'asdf'.index('d')
+assert -1 == 'asdf'.find('dd')
+try:
+    'asdf'.index('dd')
+    raise Exception
+except ValueError as e:
+    assert e.args[0] == 'substring not found'
