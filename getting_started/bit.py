14b25b977c27f500ef9d585be9afa564acedd084
AoEiuV020 <aoeiuv020@gmail.com>
2021-01-23 21:50:04 +0800

    测试乘负数求最低为1位

--- getting_started/bit.py
+++ getting_started/bit.py
@@ -7,7 +7,14 @@ assert 0b10101100 ^ 0b11001010 == 0b01100110
 
 # 反直觉，python整数没有最高位符号位，
 n = 0b1010
-assert ~n != n ^0b1111
+assert ~n != n ^ 0b1111
 assert ~n == -(n) - 1
 assert bin(10) == '0b1010'
 assert bin(~10) == bin(-11) == '-0b1011'
+w = 10
+h = 12
+assert w & 0xffff == 0b1010
+assert h & 0xffff == 0b1100
+assert +w * h & 0xffff == 0b0000000001111000
+assert -w * h & 0xffff == 0b1111111110001000
+assert w * h & -w * h & 0xffff == 2 * (2 ** 2) == 8
