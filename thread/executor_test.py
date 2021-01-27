344e1d40bfaaa5c34748853fcbe45f42d6133ccf
AoEiuV020 <aoeiuv020@gmail.com>
2021-01-27 22:09:55 +0800

    测试无阻塞线程池ThreadPoolExecutor

---
+++ thread/executor_test.py
@@ -0,0 +1,24 @@
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def increase_n(n, delay, x, r):
+    print(n)
+    time.sleep(delay)
+    n[0] += x
+    assert n[0] == r
+
+
+with ThreadPoolExecutor(max_workers=2) as executor:
+    n = [2]
+    # noinspection PyListCreation
+    futures = []
+    futures.append(executor.submit(increase_n, n, 0.3, 3, 9))
+    futures.append(executor.submit(increase_n, n, 0.2, 4, 6))
+    futures.append(executor.submit(increase_n, n, 0.4, 5, 14))
+    assert n[0] == 2
+    for t in futures:
+        t.result()
+for t in futures:
+    assert t.done()
+assert n[0] == 14
